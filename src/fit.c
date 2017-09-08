0c063b9f47108d20921340654bde0f50ccbd4a9c
Ethan Twardy <etwardy@xes-inc.com>
2017-09-08 15:19:11 -0500

    Finish implementing plot()--UNTESTED
    This commit finishes the implementation of the plot() function.
    Unit testing to follow which will mark its inclusion in a stable
    release. This function uses the gnuplot_i interface, written by
    N. Devillard. His name is mentioned wherever his source is used.
    His source is registered under the public domain, so there is no
    conflict concerning licensing.
    Also retire the fn surface_df() due to errors. I have intentions of
    fixing this function at some point in the future.

--- src/fit.c
+++ src/fit.c
@@ -30,12 +30,45 @@
  * INCLUDES
  ***/
 
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_multifit_nlinear.h>
+#include <unistd.h>
 
+#include "gnuplot_i/gnuplot_i.h"
 #include "fit.h"
+
+/*******************************************************************************
+ * MACRO DEFINITIONS
+ ***/
+
+#define COMMAND_PNG_OUTPUT			\
+  "set terminal png; "				\
+  "set output \"surface.png\"; "
+
+#define COMMAND_SCRIPT							\
+  "set title \"Multiple Polynomial Regression of Triode Characteristics\"; " \
+  "set xlabel \"Plate Voltage, Ep (V)\"; "				\
+  "set ylabel \"Grid Voltage, Eg (V)\"; "				\
+  "set zlabel \"Plate Current, Ip (V)\"; "				\
+  "%s"									\
+  "%s"
+
+#define COMMAND_FN				\
+  "f(x,y) = %2.4g*x + %2.4g*y + %2.4g*x**2 + %2.4g*y**2 + %2.4g; "
+
+#define COMMAND_PLOT				\
+  "splot '%s' using 1:2:3, f(x,y); "
+
+/*******************************************************************************
+ * STATIC FUNCTION PROTOTYPES
+ ***/
+
+static int tmp_write_data(fit_data_t * fit_data, FILE * tmpfd);
 
 /*******************************************************************************
  * API FUNCTIONS
@@ -104,7 +137,8 @@ int surface_f(const gsl_vector * x, void * data, gsl_vector * f)
  *		    feature since the jacobian entries are continuous at all
  *		    points, no memory allocation, etc.
  *
- * NOTES:	    
+ * NOTES:	    This function is currently under development. It doesn't
+ *		    work.
  ***/
 int surface_df(const gsl_vector * x, void * data, gsl_matrix * J)
 {
@@ -182,7 +216,7 @@ fit_data_t * fit_surface(fit_data_t * data, bool call)
   /* Initialize fdf structure */
   gsl_multifit_nlinear_fdf fdf = (gsl_multifit_nlinear_fdf){
     .f = surface_f,
-    .df = NULL,
+    .df = NULL, /* Use the finite difference Jacobian. */
     .fvv = NULL,
     .n = data->empirical_data->size1,
     .p = data->empirical_data->size2,
@@ -206,6 +240,103 @@ fit_data_t * fit_surface(fit_data_t * data, bool call)
   printf("status = %s\ncallback = %d\n", gsl_strerror(status), (int)call);
   
   return NULL;
+}
+
+/*******************************************************************************
+ * FUNCTION:	    plot
+ *
+ * DESCRIPTION:	    Pipes data and commands to GNUPlot to produce a .png image
+ *		    file containing a surface plot of the regression data.
+ *
+ * ARGUMENTS:	    data: (fit_data_t *) -- Structure containing data to plot.
+ *
+ * RETURN:	    int -- 0 on success, -1 otherwise.
+ *
+ * NOTES:	    This function uses the gnuplot_i GNUPlot interface, written
+ *		    by N. Devillard, 1998. This program does not re-license the
+ *		    software written by Devillard, but it does distribute it for
+ *		    ease of use.
+ ***/
+int plot(fit_data_t * data, bool png_output)
+{
+  /* Create the temp file. */
+  char tmpfn[] = "/tmp/mpr-tubeXXXXXX";
+  FILE * tmpfd = mkstemp(tmpfn);
+  if (tmpfd == NULL) return -1;
+  tmp_write_data(data, tmpfd);
+  fclose(tmpfd);
+
+  /* Create the GNUPlot commands. */
+  char *script = NULL, *fn = NULL, *plot = NULL, *total = NULL;
+  asprintf(fn, COMMAND_FN, tmpfn);
+  if (fn == NULL)
+    goto error_exit;
+
+  asprintf(plot, COMMAND_PLOT, tmpfn);
+  if (plot == NULL)
+    goto error_exit;
+
+  asprintf(script, COMMAND_SCRIPT, fn, plot);
+  if (script == NULL)
+    goto error_exit;
+
+  asprintf(total, "%s%s",
+	   png ? COMMAND_PNG_OUTPUT : "",
+	   script);
+  if (total == NULL)
+    goto error_exit;
+
+  gnuplot_ctrl * proc = gnuplot_init();
+  gnuplot_cmd(proc, total); /* Returns void, so we can only hope. */
+  gnuplot_close(proc);
+
+  /* Remove the temp file. */
+  unlink(tmpfn);
+
+  free(total);
+  free(script);
+  free(plot);
+  free(fn);
+  return 0;
+
+ error_exit: {
+    if (total != NULL) free(total);
+    if (script != NULL) free(script);
+    if (plot != NULL) free(plot);
+    if (fn != NULL) free(fn);
+    return -1;
+  }
+}
+
+/*******************************************************************************
+ * STATIC FUNCTIONS
+ ***/
+
+/*******************************************************************************
+ * FUNCTION:	    tmp_write_data
+ *
+ * DESCRIPTION:	    Write the empirical data in fit_data out to the open file.
+ *
+ * ARGUMENTS:	    fit_data: (fit_data_t *) -- the struct containing the data.
+ *		    tmpfd: (FILE *) -- open file descriptor to write to.
+ *
+ * RETURN:	    int -- 0 on success, -1 otherwise.
+ *
+ * NOTES:	    none.
+ ***/
+static int tmp_write_data(fit_data_t * fit_data, FILE * tmpfd)
+{
+  char * format = "%2.4g %2.4g %2.4g\n";
+  gsl_matrix * data = fit_data->empirical->data;
+
+  for (int i = 0; i < data->size2; i++) {
+    fprintf(tmpfd, format,
+	    gsl_matrix_get(data, i, 0),
+	    gsl_matrix_get(data, i, 1),
+	    gsl_matrix_get(data, i, 2));
+  }
+
+  return 0;
 }
 
 /******************************************************************************/
